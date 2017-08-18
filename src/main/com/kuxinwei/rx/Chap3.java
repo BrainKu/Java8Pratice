1c71d4a5010befd8980b104f68a67d9e550b6e36
Ku <kuxinwei@163.com>
2017-08-18 08:06:01 +0800

    lift 操作符

--- src/main/com/kuxinwei/rx/Chap3.java
+++ src/main/com/kuxinwei/rx/Chap3.java
@@ -2,12 +2,14 @@ package com.kuxinwei.rx;
 
 import com.sun.tools.javac.util.Pair;
 import rx.Observable;
+import rx.Subscriber;
 
 import java.util.concurrent.TimeUnit;
 
 public class Chap3 {
     public static void main(String[] args) throws InterruptedException {
-        speak(" I m the king of the world", 500).subscribe(System.out::println);
+//        speak(" I m the king of the world", 500).subscribe(System.out::println);
+        printOddString();
         Thread.sleep(10000);
     }
 
@@ -21,5 +23,44 @@ public class Chap3 {
         return contents.zipWith(wordsTime.startWith(0L), Pair::of)
                 .flatMap(stringLongPair ->
                         Observable.just(stringLongPair.fst).delay(stringLongPair.snd, TimeUnit.MILLISECONDS));
+    }
+
+    public static void printOddString() {
+        Observable.range(1, 9).lift(toStringOffOdd()).subscribe(System.out::println);
+    }
+
+    private static <T> Observable.Operator<String, T> toStringOffOdd() {
+        return new Observable.Operator<String, T>() {
+            boolean odd = true;
+
+            @Override
+            public Subscriber<? super T> call(Subscriber<? super String> subscriber) {
+                return new Subscriber<T>(subscriber) { // 这里一定要注意传入原有的 subscriber
+                    @Override
+                    public void onCompleted() {
+                        subscriber.onCompleted();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        subscriber.onError(e);
+                    }
+
+                    @Override
+                    public void onNext(T t) {
+                        try {
+                            if (odd) {
+                                subscriber.onNext(t.toString());
+                            } else {
+                                request(1);
+                            }
+                            odd = !odd;
+                        } catch (Exception e) {
+                            onError(e);
+                        }
+                    }
+                };
+            }
+        };
     }
 }
